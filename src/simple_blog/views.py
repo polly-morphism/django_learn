2e11af052c5aecc9fb8b93550033c2d7aef8ed51
Polina Verzun <polinasirko@gmail.com>
2019-07-02 13:00:10 +0300

    dynamic lookups, handling dynamic url errors

--- src/simple_blog/views.py
+++ src/simple_blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
-
+from django.http import Http404
 # Create your views here.
 
-def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+def blog_post_detail_page(request, post_id):
+    obj = get_object_or_404(BlogPost, id=post_id)
     template_name = "blog_post_detail_page.html"
     context = {"object": obj,}
     return render(request, template_name, context)
