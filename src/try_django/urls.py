a12faf9de4559779c47cd6e856df1a37f30349c0
Polina Verzun <polinasirko@gmail.com>
2019-07-02 13:28:19 +0300

    making slugs unique

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -4,7 +4,7 @@ from simple_blog.views import *
 from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/<int:post_id>/', blog_post_detail_page),
+    path('blog/<str:slug>/', blog_post_detail_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('', home_page),
